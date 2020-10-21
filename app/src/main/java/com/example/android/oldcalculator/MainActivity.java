fe3c85d06dcde592aeb1c214cf4aab3bd16cba46
Yury Petrashev <yurapetr@yahoo.com>
2020-10-21 20:15:01 +0700

    Added bindings for buttons

--- app/src/main/java/com/example/android/oldcalculator/MainActivity.java
+++ app/src/main/java/com/example/android/oldcalculator/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.android.oldcalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.android.oldcalculator.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+         binding.bt0.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+
+             }
+         });
     }
 }
